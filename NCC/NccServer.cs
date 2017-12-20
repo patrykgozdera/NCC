ea06e0b39d7d7c10f3e33f168ca02ededee71920
patrykgozdera <patryk.gozdera@gmail.com>
2017-12-20 19:02:43 +0100

    0.0.5
    połączenie z CC

--- NCC/NccServer.cs
+++ NCC/NccServer.cs
@@ -3,6 +3,7 @@ using CustomSocket;
 using System.Net;
 using System.Threading;
 using System.Net.Sockets;
+using System.Collections.Generic;
 
 namespace NCC
 {
@@ -105,6 +106,7 @@ namespace NCC
                 LogClass.Log("Received CALL CONFIRMED from NCC_2");
                 SendingManager.Init(Config.getIntegerProperty("sendPortToCC"));
                 LogClass.Log("Sending CONNECTION REQUEST to CC" + Environment.NewLine);
+                SendingManager.SendObject(new Dictionary<string, string>());
                 SendingManager.SendMessage(CONNECTION_REQUEST, firstParam, secondParam, capacity);
 
             }
